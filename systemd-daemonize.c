a1e220ba9aabb3d1c2258d807919d7a1a25dcf49
msizanoen1 <qtmlabs@protonmail.com>
2020-08-09 18:03:03 +0700

    Refactor

--- systemd-daemonize.c
+++ systemd-daemonize.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 
 #include <dlfcn.h>
+#include <errno.h>
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,45 +9,66 @@
 #include <systemd/sd-id128.h>
 #include <unistd.h>
 
-int daemon(int nochdir, int noclose) {
-  int r = 0;
+#define FATAL(...)                                                             \
+  do {                                                                         \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    abort();                                                                   \
+  } while (0)
+#define FAIL_SILENT                                                            \
+  do {                                                                         \
+    errno = 0;                                                                 \
+    return 0;                                                                  \
+  } while (0)
+#define TRY_NOFAIL(x)                                                          \
+  if (x)                                                                       \
+  FAIL_SILENT
+#define TRY(x)                                                                 \
+  if (x)                                                                       \
+  return x
+#define SDD_CLEANUP(x) __attribute__((cleanup(x##_unrefp))) x
 
+int
+daemon(int nochdir, int noclose)
+{
   /* call original daemon function */
   int (*daemon_orig)(int, int) = dlsym(RTLD_NEXT, "daemon");
-  if (daemon_orig == NULL) {
-    fprintf(stderr,
-            "systemd-daemonize: FATAL ERROR: daemon() NOT FOUND, ABORTING");
-    abort();
-  }
-  r = daemon_orig(nochdir, noclose);
-  if (r < 0)
-    return r;
+  if (daemon_orig == NULL)
+    FATAL("systemd-daemonize: FATAL ERROR: symbol \"daemon\" not found");
+  TRY(daemon_orig(nochdir, noclose));
 
-  __attribute__((cleanup(sd_bus_unrefp))) sd_bus *bus = NULL;
-  r = sd_bus_open_user(&bus);
-  if (r < 0) {
-    return 0; /* fail silently if cannot connect to dbus */
-  }
+  SDD_CLEANUP(sd_bus)* bus = NULL;
+  TRY_NOFAIL(sd_bus_open_user(&bus));
+
+  sd_id128_t scope_rand = SD_ID128_NULL;
+  TRY_NOFAIL(sd_id128_randomize(&scope_rand));
 
   char unit_name[512];
-  pid_t pid = getpid();
-  sd_id128_t scope_rand = SD_ID128_NULL;
-  r = sd_id128_randomize(&scope_rand);
-  if (r < 0)
-    return 0;
-  sprintf(unit_name, "systemd-daemonize-r" SD_ID128_FORMAT_STR ".scope",
+  sprintf(unit_name,
+          "systemd-daemonize-r" SD_ID128_FORMAT_STR ".scope",
           SD_ID128_FORMAT_VAL(scope_rand));
 
-  __attribute__((cleanup(sd_bus_message_unrefp))) sd_bus_message *reply = NULL;
+  SDD_CLEANUP(sd_bus_message)* reply = NULL;
   sd_bus_error error = SD_BUS_ERROR_NULL;
-  r = sd_bus_call_method(
-      bus, "org.freedesktop.systemd1", "/org/freedesktop/systemd1",
-      "org.freedesktop.systemd1.Manager", "StartTransientUnit", &error, &reply,
-      "ssa(sv)a(sa(sv))", unit_name, "fail", 2, "PIDs", "au", 1, (uint32_t)pid,
-      "Slice", "s", "systemd_daemonize.slice", 0);
-  if (r < 0) {
-    return 0;
-  }
+
+  TRY_NOFAIL(sd_bus_call_method(bus,
+                                "org.freedesktop.systemd1",
+                                "/org/freedesktop/systemd1",
+                                "org.freedesktop.systemd1.Manager",
+                                "StartTransientUnit",
+                                &error,
+                                &reply,
+                                "ssa(sv)a(sa(sv))",
+                                unit_name,
+                                "fail",
+                                2,
+                                "PIDs",
+                                "au",
+                                1,
+                                (uint32_t)getpid(),
+                                "Slice",
+                                "s",
+                                "systemd_daemon.slice",
+                                0));
 
   return 0;
 }
